ac4add5113a6b1413b0d4bb7f18636ac8a519417
Syed <moddin1989@gmail.com>
2022-01-16 20:21:53 +0530

    coding question 2

---
+++ interview/example-2.js
@@ -0,0 +1,11 @@
+//parenthis which are valid one
+var longestValidParentheses = function (S) {
+  let stack = [-1],
+    ans = 0
+  for (let i = 0; i < S.length; i++)
+    if (S[i] === '(') stack.push(i)
+    else if (stack.length === 1) stack[0] = i
+    else stack.pop(), (ans = Math.max(ans, i - stack[stack.length - 1]))
+  return ans
+}
+console.log(longestValidParentheses(')(()(()))'))
